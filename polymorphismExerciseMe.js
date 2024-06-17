ef39b84d664ee7c8ee4c5fe80cbff9be6ef4e763
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-17 12:30:25 +0200

    add poly exercise

--- polymorphismExerciseMe.js
+++ polymorphismExerciseMe.js
@@ -41,7 +41,7 @@ function HtmlImageElement(src) {
     this.src = src
 
     this.render = function () {
-        console.log(`<img src=${this.src} />`)
+        console.log(`<img src=${src} />`)
     }
 }
 
