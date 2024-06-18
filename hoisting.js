eb70239d486fbb96bac8419ec1cbadac3cedf48a
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-18 15:54:42 +0200

    add hoisting

---
+++ hoisting.js
@@ -0,0 +1,19 @@
+// Function Declaration
+// Hoisted
+function sayHello() { }
+
+// Function Expression (add ; to end!)
+// Not Hoisted
+const sayGoodbye = function () { };
+
+// Class Declaration
+// Not Hoisted
+class Circle {
+
+}
+
+// Class Expression (add ; to end!)
+// Not Hoisted
+const square = class {
+
+};
